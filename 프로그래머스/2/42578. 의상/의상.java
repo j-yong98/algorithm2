5170bd1196631889800cd746e2b09d05a6d49254
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2023-12-12 16:37:14 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 1) + 1);
+        }
+        int answer = 1;
+        for (String key : map.keySet()) {
+            answer *= map.get(key);
+        }
+        return answer - 1;
+    }
+}
