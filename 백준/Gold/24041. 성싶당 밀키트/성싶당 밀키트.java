ad84eebcee71581999c8b00e1fb9b8811e93b6ee
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-02-28 14:56:12 +0900

    [Gold IV] Title: 성싶당 밀키트, Time: 2392 ms, Memory: 225312 KB -BaekjoonHub

--- 백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.java
+++ 백준/Gold/24041. 성싶당 밀키트/성싶당 밀키트.java
@@ -25,29 +25,27 @@ public class Main {
 
         Arrays.sort(foods, (a, b) -> a[1] == b[1] ? -Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
 
-        long ans = getAns();
-        System.out.println(ans);
-        br.close();
-    }
-
-    private static long getAns() {
         long left = 0;
         long right = MAX;
         long ans = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>();
         while (left <= right) {
-            long mid = (left + right) / 2;
+            long mid = (left + right) >> 1;
             long sum = 0;
-            List<Long> exclude = new ArrayList<>();
             for (int i = 0; i < N; i++) {
                 long res = getGerm(foods[i][0], foods[i][1], mid);
-                if (foods[i][2] == 1L) {
-                    exclude.add(res);
+                if (foods[i][2] == 1) {
+                    if (pq.size() < K) {
+                        pq.add(res);
+                    } else if (!pq.isEmpty() && pq.peek() < res){
+                        pq.add(res);
+                        pq.poll();
+                    }
                 }
                 sum += res;
             }
-            exclude.sort(Comparator.reverseOrder());
-            for (int i = 0; i < Math.min(exclude.size(), K); i++) {
-                sum -= exclude.get(i);
+            while (!pq.isEmpty()) {
+                sum -= pq.poll();
             }
             if (sum <= G) {
                 left = mid + 1;
@@ -56,7 +54,8 @@ public class Main {
                 right = mid - 1;
             }
         }
-        return ans;
+        System.out.println(ans);
+        br.close();
     }
 
     private static long getGerm(long s, long l, long day) {
