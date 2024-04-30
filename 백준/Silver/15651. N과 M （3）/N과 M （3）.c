48e55e0d67c441819036adec395c25b814544aef
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-04-30 22:30:04 +0900

    [Silver III] Title: N과 M (3), Time: 496 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int arr[8];
+void f(int l, int n, int m) {
+	if (l == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 1; i <= n; i++) {
+		arr[l] = i;
+		f(l + 1, n, m);
+	}
+}
+
+int main() {
+	int n, m;
+
+	scanf("%d %d", &n, &m);
+	f(0, n, m);
+	return 0;
+}
