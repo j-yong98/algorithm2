7d059e42b30bdee88908ad33a745ae84c932207a
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-05-22 00:42:13 +0900

    [Gold IV] Title: 인접한 비트의 개수, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.java
+++ 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,7 +8,6 @@ public class Main {
     static int T;
     static int N, K;
     static int[][][] dp = new int[MAX + 5][MAX + 5][2];
-    static int[] check = new int[MAX];
 
     /**
      * 인접한 비트의 개수
@@ -18,16 +16,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= MAX; i++) {
-            for (int j = 0; j <= MAX; j++) {
-                Arrays.fill(dp[i][j], -1);
+        dp[1][0][0] = 1;
+        dp[1][0][1] = 1;
+
+        for (int i = 2; i <= MAX; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                if (j == 0) {
+                    dp[i][j][1] = dp[i-1][j][0];
+                } else {
+                    dp[i][j][1] = dp[i-1][j][0] + dp[i-1][j-1][1];
+                }
             }
         }
         for (int t = 0; t < T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-            sb.append(f(N - 1, K, 0)).append("\n");
+            sb.append(dp[N][K][0] + dp[N][K][1]).append("\n");
         }
         System.out.println(sb);
         br.close();
@@ -47,9 +53,7 @@ public class Main {
 
         dp[n][k][prev] = f(n - 1, k, 0);
         if (!(k == 0 && prev == 1)) {
-            check[n] = 1;
             dp[n][k][prev] += f(n - 1, k - prev, 1);
-            check[n] = 0;
         }
         return dp[n][k][prev];
     }
