06ddad8bab9390526d4860a222cff738481429ef
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-07-08 11:45:27 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 652 ms, Memory: 51100 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> enter = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+            if (status.equals("enter")) {
+                enter.add(name);
+            } else {
+                enter.remove(name);
+            }
+        }
+        List<String> names = new ArrayList<>(enter);
+        names.sort((a, b) -> b.compareTo(a));
+        StringBuilder sb = new StringBuilder();
+        names.forEach(s -> sb.append(s).append("\n"));
+        System.out.print(sb);
+    }
+}
