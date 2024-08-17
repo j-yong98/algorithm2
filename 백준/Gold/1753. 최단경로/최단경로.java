a3fa079dc6a7d48c5ae3507a1422279175e30e03
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-08-17 12:45:25 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 109396 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -8,69 +8,63 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final int MAX = 100_000_000;
-	static int v, e;
-	static int start;
-	static List<Node>[] weights;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		v = Integer.parseInt(st.nextToken());
-		e = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(br.readLine());
-		weights = new List[v + 1];
-		for (int i = 1; i <= v; i++)
-			weights[i] = new ArrayList<>();
-		
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			weights[a].add(new Node(b, c));
-		}
-		shortestPath();
-	}
-	
-	
-	private static void shortestPath() {
-		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-		boolean[] visited = new boolean[v + 1];
-		int[] dist = new int[v + 1];
-		Arrays.fill(dist, MAX);
-		
-		dist[start] = 0;
-		pq.add(new Node(start, 0));
-		
-		while (!pq.isEmpty()) {
-			Node node = pq.poll();
-			
-			if (!visited[node.idx])
-				visited[node.idx] = true;
-			
-			for (Node next : weights[node.idx]) {
-				if (visited[next.idx]) continue;
-				if (node.weight + next.weight < dist[next.idx]) {
-					dist[next.idx] = node.weight + next.weight;
-					pq.add(new Node(next.idx, dist[next.idx]));
-				}
-			}
-		}
-		
-		for (int i = 1; i <= v; i++) {
-			sb.append(dist[i] >= MAX ? "INF" : dist[i]).append("\n");
-		}
-		System.out.println(sb);
-	}
-	
-	static class Node{
-		int idx;
-		int weight;
-		public Node(int idx, int weight) {
-			super();
-			this.idx = idx;
-			this.weight = weight;
-		}
-	}
+    static int V, E;
+    static int K;
+    static List<List<Node>> edges = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        for (int i = 0; i <= V; i++) {
+            edges.add(new ArrayList<>());
+        }
+        K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges.get(u).add(new Node(v, w));
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.w, b.w));
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dist[K] = 0;
+        pq.add(new Node(K, 0));
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (now.w > dist[now.v]) {
+                continue;
+            }
+
+            for (Node next : edges.get(now.v)) {
+                if (next.w + now.w >= dist[next.v]) {
+                    continue;
+                }
+                dist[next.v] = now.w + next.w;
+                pq.add(new Node(next.v, dist[next.v]));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= V; i++) {
+            sb.append(dist[i] == Integer.MAX_VALUE ? "INF": dist[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static class Node {
+        int v;
+        int w;
+
+        public Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+    }
+
 }
