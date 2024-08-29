64bb9b5f93d00ee2e67559ffcb7fbf8e544c23cb
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2024-08-29 11:22:48 +0900

    [Silver I] Title: 한강 (Small), Time: 4896 ms, Memory: 23272 KB -BaekjoonHub

--- 백준/Silver/12435. 한강 （Small）/한강 （Small）.java
+++ 백준/Silver/12435. 한강 （Small）/한강 （Small）.java
@@ -1,18 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static final int MAX_CNT = 250;
     static final int MAX = 1_000_000;
     static int T;
     static int N, M;
-    static List<List<Integer>> bros = new ArrayList<>();
-    static List<List<Integer>> sisters = new ArrayList<>();
+    static int[] cnt = new int[MAX + 1];
+    static int[] bro = new int[MAX + 1];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         init();
@@ -22,53 +18,35 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
-            int cnt = 0;
-            int d = bros.get(N).size();
-            // 자매관계(약수의 관계가 같다)이면서 그들의 남동생이 M 이상인 경우
-            for (int cur : sisters.get(d)) {
-                if (cur >= N) break;
-                if (!bros.get(cur).isEmpty() && bros.get(cur).get(0) >= M) {
-                    cnt++;
+            int ans = 0;
+            for (int i = 2; i < N; i++) {
+                if (cnt[N] == cnt[i] && bro[i] >= M) {
+                    ans++;
                 }
             }
-            sb.append("Case #").append(t + 1).append(": ").append(cnt).append("\n");
+            sb.append("Case #").append(t + 1).append(": ").append(ans).append("\n");
         }
         System.out.print(sb);
     }
 
     private static void init() {
-        for (int i = 0; i <= MAX; i++) {
-            bros.add(new ArrayList<>());
-        }
-        for (int i = 0; i < MAX_CNT; i++) {
-            sisters.add(new ArrayList<>());
-        }
-
-
-        // 형제, 자매 구하기
         for (int i = 2; i <= MAX; i++) {
-            int cnt = divisor(i);
-            sisters.get(cnt).add(i);
-        }
-
-        for (List<Integer> s : sisters) {
-            Collections.sort(s);
+            divisor(i);
         }
     }
 
-    private static int divisor(int val) {
-        int cnt = 0;
+    private static void divisor(int val) {
+        bro[val] = Integer.MAX_VALUE;
         for (int i = 2; i <= Math.sqrt(val); i++) {
             if (val % i == 0) {
-                bros.get(val).add(i);
+                bro[val] = Math.min(bro[val], i);
                 if (i * i == val) {
-                    cnt += 1;
+                    cnt[val] += 1;
                 } else {
-                    bros.get(val).add(val / i);
-                    cnt += 2;
+                    cnt[val] += 2;
                 }
             }
         }
-        return cnt;
+        bro[val] = bro[val] == Integer.MAX_VALUE ? 0 : bro[val];
     }
 }
