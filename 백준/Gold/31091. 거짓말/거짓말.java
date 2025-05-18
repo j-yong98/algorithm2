fed9c42428a77ec9758ef5d4e0cc5fc87227b666
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2025-05-18 12:44:41 +0900

    [Gold IV] Title: 거짓말, Time: 656 ms, Memory: 105144 KB -BaekjoonHub

---
+++ 백준/Gold/31091. 거짓말/거짓말.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] sum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        sum = new int[N + 2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            if (x > 0) { // x 명 이상 -> x 명 미만을 거짓 (0 ~ x - 1 까지)
+                sum[0] += 1;
+                sum[x] -= 1;
+            } else { // x 명 이하 -> x 명 초과는 거짓 (x + 1 ~ N까지)
+                x = -x;
+                sum[x + 1] += 1;
+                sum[N + 1] -= 1;
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sum[i] += sum[i - 1];
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            if (i == sum[i]) {
+                ans.add(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(ans.size()).append("\n");
+        for (Integer i : ans) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+}
