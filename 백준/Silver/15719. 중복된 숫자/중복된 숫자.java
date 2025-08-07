ad06b752d6cf49f343104807b5058e38e9eda1f5
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2025-08-07 20:38:59 +0900

    [Silver V] Title: 중복된 숫자, Time: 2204 ms, Memory: 480820 KB -BaekjoonHub

---
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[num]++;
+            if (arr[num] == 2) {
+                System.out.println(num);
+                return;
+            }
+        }
+    }
+
+}
