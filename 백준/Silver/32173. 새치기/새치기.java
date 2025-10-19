6f0d6fc20b0730371336dfbc0f55717a1f4a8ae4
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2025-10-19 12:50:23 +0900

    [Silver III] Title: 새치기, Time: 300 ms, Memory: 27936 KB -BaekjoonHub

---
+++ 백준/Silver/32173. 새치기/새치기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long sum = 0;
+        long ans = 0;
+        for (int i = 0; i < N; i++) {
+            ans = Math.max(ans, arr[i] - sum);
+            sum += arr[i];
+        }
+        System.out.println(ans);
+    }
+}
