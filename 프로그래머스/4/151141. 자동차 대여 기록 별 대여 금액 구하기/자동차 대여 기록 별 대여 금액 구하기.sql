db273911ff43383f6299a2d266daed0342973b8f
j-yong98 <120557342+j-yong98@users.noreply.github.com>
2026-02-25 18:04:36 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,20 +1,19 @@
-select h.history_id,  (1 - nvl((
-        select 
-            case    
-                when h.end_date - h.start_date + 1 >= 90 then discount_rate
-                when h.end_date - h.start_date + 1 >= 30 then discount_rate
-                when h.end_date - h.start_date + 1 >= 7  then discount_rate
-            end
-        from CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
-        where p.car_type = c.car_type
-        and (
-            (p.duration_type = '7일 이상'  and h.end_date - h.start_date + 1 >= 7  and h.end_date - h.start_date + 1 < 30) or
-            (p.duration_type = '30일 이상' and h.end_date - h.start_date + 1 >= 30 and h.end_date - h.start_date + 1 < 90) or
-            (p.duration_type = '90일 이상' and h.end_date - h.start_date + 1 >= 90)
+select h.history_id, (1 - nvl(
+    (
+    select p.discount_rate / 100
+    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN p
+    where p.duration_type = (
+        case
+            when h.end_date - h.start_date + 1 >= 7 and h.end_date - h.start_date + 1 < 30 then '7일 이상'
+            when h.end_date - h.start_date + 1 >= 30 and h.end_date - h.start_date + 1 < 90 then '30일 이상'
+            when h.end_date - h.start_date + 1 >= 90 then '90일 이상'
+        else null
+        end
         )
-    ), 0) / 100) * c.daily_fee * (h.end_date - h.start_date + 1) fee
-from CAR_RENTAL_COMPANY_CAR c, CAR_RENTAL_COMPANY_RENTAL_HISTORY h 
-where h.car_id = c.car_id(+)
-and c.car_type = '트럭'
-order by fee desc, h.history_id desc
-
+    and p.car_type = c.car_type
+    ), 0)
+) * c.DAILY_FEE * (h.end_date - h.start_date + 1) fee
+from CAR_RENTAL_COMPANY_CAR c, CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+where c.car_type = '트럭'
+and h.car_id = c.car_id
+order by fee desc, history_id desc
